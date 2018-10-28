2dee7096dba421a1ffbf2eed77d2b32df5891ba8
Alex Kharouk <akharoukpp@gmail.com>
2018-10-28 12:48:02 +0000

    solve controlflow + create executable

--- chapter3-JavaBuzz/exercises/ControlFlow.java
+++ chapter3-JavaBuzz/exercises/ControlFlow.java
@@ -1,10 +1,20 @@
 // what imports will you need?
-
+import java.util.Scanner;
 // can you remember how to set up your main function?
-
-// find a way to read in user input
-
-// write a method that will check if a number is odd or even
-// (assume user only ever enters integers)
-
-// print the answer to the console
+public class ControlFlow {
+  public static void main(String[] args) {
+  // find a way to read in user input
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please give me a number: ");
+    int number = scan.nextInt();
+  // write a method that will check if a number is odd or even
+  // (assume user only ever enters integers)
+    if (number % 2 == 0) {
+      // print the answer to the console
+      System.out.println("Your number " + number + " is even.");
+    } else {
+      // print the answer to the console
+      System.out.println("Your number " + number + " is odd.");
+    }
+  }
+}
