20c4b6f81bed19a8fbc0ce34034d2d251858b7a1
Alex Kharouk <akharoukpp@gmail.com>
2018-10-28 13:07:58 +0000

    solve fizzbuzz in java

--- chapter3-JavaBuzz/exercises/JavaBuzz.java
+++ chapter3-JavaBuzz/exercises/JavaBuzz.java
@@ -1,5 +1,26 @@
-// can you remember how to set up your main function?
+import java.util.ArrayList;
 
+public class JavaBuzz {
+  public static void main(String[] args) {
+    ArrayList<Integer> javaBuzzAry = new ArrayList<Integer>(100);
+    for (int i = 1; i < 101; i++) {
+      javaBuzzAry.add(i);
+    }
+    for (int i = 0; i < javaBuzzAry.size(); i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        System.out.println("JavaBuzz");
+      } 
+      else if (i % 3 == 0) {
+        System.out.println("Java");
+      }
+      else if (i % 5 == 0) {
+        System.out.println("Buzz");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+}
 // "Java" if divisible by 3
 // "Buzz" if divisible by 5
 // "JavaBuzz" if divisible by 15
