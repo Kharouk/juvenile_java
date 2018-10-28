00f04cf76f3fd313627fb43e43e0d0b8b1552cbe
Alex Kharouk <akharoukpp@gmail.com>
2018-10-28 11:12:27 +0000

    complete strings exercise

--- chapter2-DataTypes/exercises/Strings.java
+++ chapter2-DataTypes/exercises/Strings.java
@@ -1,12 +1,12 @@
 public class Strings {
   public static void main(String[] args) {
     // declare the first name
-
+    String firstString = "Alex";
     // declare the second name
-
+    String secondString = "Carol";
     // create your message
-
+    String message = String.format("%s and %s, sitting in a tree!", firstString, secondString);
     // print your message!
-
+    System.out.println(message);
   }
 }
